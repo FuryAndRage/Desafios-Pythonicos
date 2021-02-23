79ca14550c0241add7452a62e6370b1fcf0a21e6
Moises Andrade <lawliex@gmail.com>
2021-02-24 01:02:11 +1300

    solving mix_up exercise

--- 04_mix_up.py
+++ 04_mix_up.py
@@ -15,7 +15,6 @@ Assuma que a e b tem tamanho 2 ou maior.
 def mix_up(a, b):
     return f'{b[:2]}{a[2:]} {a[:2]}{b[2:]}'
 
-
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
 
 def test(f, in_, expected):
