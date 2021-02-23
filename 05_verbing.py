d2667dc1a579ae11cdaed6dce6b189d6062f9cab
Moises Andrade <lawliex@gmail.com>
2021-02-24 01:05:10 +1300

    solving verbing exercise

--- 05_verbing.py
+++ 05_verbing.py
@@ -11,7 +11,7 @@ Retorne o resultado da string.
 """
 
 def verbing(s):
-    if len(s) > 3:
+    if len(s) >= 3:
         return f'{s + "ing" if not "ing" in s[-3:] else s + "ly" }'
     else:
         return s
